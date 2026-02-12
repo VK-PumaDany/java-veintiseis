13e6fc023de7a0dc691a18b3cf92921628a38ed5
VakanoDan <vakanodan@gmail.com>
2026-02-11 21:20:26 -0500

    feat: add name and categories

--- logitech_soluciones/App.java
+++ logitech_soluciones/App.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+
 import src.products.Products;
 
 public class App {
     public static void main(String[] args) {
+        ArrayList<Products> listProducts = new ArrayList<Products>();
+
         // Al estar en la misma carpeta, App ya sabe quién es Products
         Products cellPhone = new Products();
         cellPhone.create_product();
+
+        listProducts.add(cellPhone);
+
+        System.out.print(listProducts);
+
     }
 }
--- logitech_soluciones/src/products/Products.java
+++ logitech_soluciones/src/products/Products.java
@@ -1,8 +1,20 @@
 package src.products;
 
+import java.util.Scanner;
+
 public class Products {
     public void create_product() {
+        Scanner scan = new Scanner(System.in);
+        // Atributos
         double unique_code = Math.random();
+
         System.out.println("Código generado: " + unique_code);
+
+        System.out.println("Ingrese el nombre del producto: ");
+        String name_product = scan.nextLine();
+        System.out.println("Ingrese el nombre de la categoria: ");
+        String category = scan.nextLine();
+
+        scan.close();
     }
 }
